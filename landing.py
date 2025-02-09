f25e54efde89fab1689d4208e739f40f62b07116
Justin Son <js11749@nyu.edu>
2025-02-08 21:40:13 -0500

    Name

--- landing.py
+++ landing.py
@@ -9,31 +9,45 @@ load_dotenv()
 API_KEY = "ad96e72d43ad9c0a3e39125921b15882"
 BASE_URL = "http://api.nessieisreal.com"
 HEADERS = {"Content-Type": "application/json"}
-
 CUSTOMER_ID = ["67a7aa2f9683f20dd518bc17", "67a7e5fb9683f20dd518bdea"]
 
-
-
-# ✅ Check Credentials (Replace with secure authentication)
+# Check Credentials (Replace with secure authentication)
 def check_credentials(username, password):
     return username in CUSTOMER_ID and password == "p"
 
-
-# ✅ Initialize session state
+# Initialize session state
 if "logged_in" not in st.session_state:
     st.session_state["logged_in"] = False
 
-# 🔹 Login Form
+# Login Form
 if not st.session_state["logged_in"]:
     st.image("logo.svg", width=800)
-    st.title("Login")
+    st.markdown(
+        """
+        <style>
+        @font-face {
+            font-family: 'FrutigerBlackItalic';
+            src: url('FrutigerBlackItalic.ttf') format('truetype');
+        }
+        .centered-title {
+            text-align: center;
+            font-family: 'FrutigerBlackItalic';
+            color: rgb(210, 46, 30);
+        }
+        </style>
+        <div class="centered-title">
+            <h1>Loan Advisor</h1>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
     username = st.text_input("Capital One ID")
     password = st.text_input("Password", type="password", value = "p")
     aiText = "Optional: You can enable the Financial Advisor Chatbot with your OpenAI API Key"
     openAIKey = st.text_input("OpenAI API Key", placeholder = aiText)
 
-    if st.button("Login"):
+    if st.button("Sign in"):
         if check_credentials(username, password):
             st.session_state["logged_in"] = True
             st.success("Logged in successfully!")
