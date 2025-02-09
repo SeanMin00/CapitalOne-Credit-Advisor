33b6c657eaa851836e2250c217e598c6df2cd6d0
hj2116 <78177761+hj2116@users.noreply.github.com>
2025-02-09 09:52:34 -0500

    Delete lo.py

--- lo.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-if st.button("Log in"):
-    st.login("auth0")
-if st.experimental_user.is_logged_in:
-    if st.button("Log out"):
-        st.logout()
-    st.write(f"Hello, {st.experimental_user.name}!")
